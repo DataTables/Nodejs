0aab81c9133665fbff0ea41c59aa069810f9bae7
Allan Jardine <allan@datatables.net>
2026-09-11 12:36:49 +0000

    Release 3.0.2

--- src/editor.ts
+++ src/editor.ts
@@ -68,7 +68,7 @@ export enum Action {
 export default class Editor extends NestedData {
 	public static Action = Action;
 
-	public static version: string = '3.0.1';
+	public static version: string = '3.0.2';
 
 	/**
 	 * Determine the request type from an HTTP request.
